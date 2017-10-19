e49ee887cd02d17548763b88bad3306d0d180f30
Steven Hein <steven.c.hein@gmail.com>
2017-10-19 18:58:09 -0400

    Adding all scenes

---
+++ Assets/Scripts/gameOverManager.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+private var restartBtn : UnityEngine.UI.Button;
+
+function Awake () {
+    restartBtn = GameObject.Find("btn_RestartBtn").GetComponent.<UnityEngine.UI.Button>();
+    restartBtn.onClick.AddListener(restartGame);
+}
+
+function restartGame () {
+	SceneManagement.SceneManager.LoadScene("Main");
+}
+
--- Assets/Scripts/loader.js
+++ Assets/Scripts/loader.js
@@ -1,9 +1,19 @@
 ﻿#pragma strict
 
-var gameManager : gameManager;
+var boardScript : boardManager;
+private var startBtn : UnityEngine.UI.Button;
 
 function Awake () {
-    if (gameManager.instance == null) {
-        Instantiate(gameManager);
-    }
+    startBtn = GameObject.Find("btn_StartBtn").GetComponent.<UnityEngine.UI.Button>();
+    startBtn.onClick.AddListener(startGame);
+
+    for (var i : int = 0; i < 15; i++) {
+		var randPosition : Vector3 = boardScript.getRandomPositionOnBoard();
+		boardScript.spawnEnemyAt(randPosition);
+	}
 }
+
+function startGame () {
+	SceneManagement.SceneManager.LoadScene("Main");
+}
+
