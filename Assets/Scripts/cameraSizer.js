0c0c0fcc8ef661241b6210d91bb52e292497e918
Steven Hein <steven.c.hein@gmail.com>
2017-11-19 22:41:20 +0100

    Add sizer

--- Assets/Scripts/cameraSizer.js
+++ Assets/Scripts/cameraSizer.js
@@ -2,11 +2,22 @@
 
 var pixelsToUnits : float;
 var cameraComponent : Camera;
+var resolutionChanged : boolean = false;
+
+private var height : float;
+
+function Awake () {
+	height = Screen.height;
+}
 
 function Start () {
 	cameraComponent = GetComponent.<Camera>();
 }
 
 function Update () {
+	if (Screen.height != height) {
+		resolutionChanged = true;
+		height = Screen.height;
+	}
  	cameraComponent.orthographicSize = Screen.height / pixelsToUnits / 2;
 }
